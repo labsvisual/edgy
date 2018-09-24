55dcac78a6ce65b0c9e7e5f3752e129b757bd163
Shreyansh Pandey <s@isomr.co>
2018-09-24 17:49:03 +0530

    added tests for all errors

--- tests/Errors.tests.js
+++ tests/Errors.tests.js
@@ -1,9 +1,18 @@
-import { Errors } from './src';
 import test from 'ava';
+import { Errors } from '../src';
 
 test( 'should contain all errors', t => {
 
     t.is( typeof Errors.InvalidResourceError, 'function' );
-    t.truthy( Errors.InvalidResourceError instanceof Error );
+    t.truthy( Errors.InvalidResourceError );
+
+    t.is( typeof Errors.InvalidAdapterConfigurationError, 'function' );
+    t.truthy( Errors.InvalidAdapterConfigurationError );
+
+    t.is( typeof Errors.InvalidUrlError, 'function' );
+    t.truthy( Errors.InvalidUrlError );
+
+    t.is( typeof Errors.NoConfigurationOptionsError, 'function' );
+    t.truthy( Errors.NoConfigurationOptionsError );
 
 } );
